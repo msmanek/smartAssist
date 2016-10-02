3f5988e059892a1f038b4ea5a7be8be96f9e3b88
MohammedShabar Manek <shabbar_manek@live.com>
2016-10-02 09:32:34 -0400

    cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 Myo.connect();
 var stepCounter = 1;
 
-
 //Whenever we get a pose event, we'll update the image sources with the active version of the image
 Myo.on('pose', function(pose){
     console.log(pose);
@@ -16,29 +15,7 @@ Myo.on('pose', function(pose){
         stepCounter--;
     }
     console.log(stepCounter);
-    // $('img.' + pose).attr('src', 'img/' + pose + '_active.png');
-    // $('.mainPose img').attr('src', 'img/' + pose + '_active.png');
 });
-
-//Opposite of above. We also revert the main img to the unlocked state
-Myo.on('pose_off', function(pose){
-    // $('img.' + pose).attr('src', 'img/' + pose + '.png');
-    // $('.mainPose img').attr('src', 'img/unlocked.png');
-});
-
-
-//Whenever a myo locks we'll switch the main image to a lock image
-Myo.on('locked', function(){
-    // $('.mainPose img').attr('src', 'img/locked.png');
-});
-
-//Whenever a myo unlocks we'll switch the main image to a unlock image
-Myo.on('unlocked', function(){
-    // $('.mainPose img').attr('src', 'img/unlocked.png');
-});
-
-
-
 
 $(document).ready(function () {
     $('html').keydown(function(e) {
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is an application made for the Next 36 Startup Sprint 2016. It provides feedback and prompts for surgeons during surgeries to help them avoid making errors.
